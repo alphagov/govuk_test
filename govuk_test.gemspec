85122beb7b624070cd144041e0854c93d3fe64a0
Tijmen Brommet <tijmen@gmail.com>
2018-07-18 12:54:44 +0100

    Add initial dependencies and config
    This will set up the app to use Chrome for javascript tests
    (https://github.com/alphagov/collections/pull/774).

--- govuk_test.gemspec
+++ govuk_test.gemspec
@@ -22,6 +22,11 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.add_dependency "capybara"
+  spec.add_dependency "chromedriver-helper"
+  spec.add_dependency "puma"
+  spec.add_dependency "selenium-webdriver"
+
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
 end
--- lib/govuk_test.rb
+++ lib/govuk_test.rb
@@ -1,5 +1,23 @@
 require "govuk_test/version"
 
+require "capybara"
+require "puma"
+require "selenium-webdriver"
+
 module GovukTest
-  # Your code goes here...
+  def self.configure
+    Capybara.register_driver :headless_chrome do |app|
+      capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+        chromeOptions: { args: %w(headless disable-gpu) }
+      )
+
+      Capybara::Selenium::Driver.new(
+        app,
+        browser: :chrome,
+        desired_capabilities: capabilities
+      )
+    end
+
+    Capybara.javascript_driver = :headless_chrome
+  end
 end
