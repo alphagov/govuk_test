c42c2ac4214cd03360ac7b7c986fc49d99c8a95a
Kevin Dew <kevindew@me.com>
2018-11-14 13:02:23 +0000

    Revert "Don't force use of chromedriver-helper"

--- govuk_test.gemspec
+++ govuk_test.gemspec
@@ -24,7 +24,6 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "capybara"
   spec.add_dependency "chromedriver-helper"
-  spec.add_dependency "ptools"
   spec.add_dependency "puma"
   spec.add_dependency "selenium-webdriver"
 
--- lib/govuk_test.rb
+++ lib/govuk_test.rb
@@ -1,22 +1,14 @@
 require "govuk_test/version"
 
 require "capybara"
-require "ptools"
 require "puma"
+require "chromedriver-helper"
 require "selenium-webdriver"
 
 module GovukTest
   def self.configure(options = {})
     chrome_options = %w(headless disable-gpu)
     chrome_options << "--window-size=#{options[:window_size]}" if options[:window_size]
-
-    chromedriver_from_path = File.which("chromedriver")
-    if chromedriver_from_path
-      # Use the installed chromedriver, rather than chromedriver-helper
-      Selenium::WebDriver::Chrome.driver_path = chromedriver_from_path
-    else
-      require 'chromedriver-helper'
-    end
 
     Capybara.register_driver :headless_chrome do |app|
       capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
