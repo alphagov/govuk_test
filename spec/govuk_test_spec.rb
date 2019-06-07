bff6189b1a8046b4913824d92048970fe7ecf762
Alan Gabbianelli <alan.gabbianelli@digital.cabinet-office.gov.uk>
2019-06-07 12:41:01 +0100

    Allow `.configure` to accept extra chrome options
    This is to be able to pass options like `chrome_options: { w3c: false }`
    
    Trello card: 
    https://trello.com/c/RNxnxVVy/1056-fix-chromedriver-issues-in-datagovukfind

--- spec/govuk_test_spec.rb
+++ spec/govuk_test_spec.rb
@@ -18,14 +18,25 @@ RSpec.describe GovukTest do
   end
 
   context ".configure with options" do
-    let(:window_size) { "1366,768" }
-    let(:expected_args) { default_args << "--window-size=#{window_size}" }
+    context "with :window_size" do
+      let(:window_size) { "1366,768" }
+      let(:expected_args) { default_args << "--window-size=#{window_size}" }
 
-    it "appends the :window_size option to default options" do
-      expect(Selenium::WebDriver::Remote::Capabilities).to receive(:chrome)
-        .with(chromeOptions: { args: expected_args })
+      it "appends the :window_size option to default options" do
+        expect(Selenium::WebDriver::Remote::Capabilities).to receive(:chrome)
+          .with(chromeOptions: { args: expected_args })
 
-      GovukTest.configure(window_size: window_size)
+        GovukTest.configure(window_size: window_size)
+      end
+    end
+
+    context "with :chrome_options" do
+      it "merges the :chrome_options option to the :chromeOptions" do
+        expect(Selenium::WebDriver::Remote::Capabilities).to receive(:chrome)
+          .with(chromeOptions: { args: default_args, some_key: 'some value' })
+
+        GovukTest.configure(chrome_options: { some_key: 'some value' })
+      end
     end
   end
 end
